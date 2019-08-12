ddb750dec9e4385dfec081c8fd65339c7374ba20
Seth Lewis <lewiscatenary@gmail.com>
2019-08-11 22:34:11 -0500

    refactored for switch statement

--- switch/switch.go
+++ switch/switch.go
@@ -4,30 +4,59 @@ import "fmt"
 
 func main() {
 	for day := 1; day <= 12; day++ {
-		if day == 12 {
+		switch day {
+		case 12:
 			fmt.Println("12 Drummers Drumming")
-		} else if day == 11 {
+		case 11:
 			fmt.Println("11 pipers piping")
-		} else if day == 10 {
+		case 10:
 			fmt.Println("Ten Lords a Leaping")
-		} else if day == 9 {
+		case 9:
 			fmt.Println("Nine Ladies Dancing")
-		} else if day == 8 {
+		case 8:
 			fmt.Println("Eight Maids a Milking")
-		} else if day == 7 {
-			fmt.Println("Seven Swans a Swinng")
-		} else if day == 6 {
+		case 7:
+			fmt.Println("Seven Swans a Swimming")
+		case 6:
 			fmt.Println("Six Geese a Laying")
-		} else if day == 5 {
+		case 5:
 			fmt.Println("Five Golden Rings")
-		} else if day == 4 {
+		case 4:
 			fmt.Println("Four Calling Birds")
-		} else if day == 3 {
+		case 3:
 			fmt.Println("Three French Hens")
-		} else if day == 2 {
+		case 2:
 			fmt.Println("Two Turtle Doves")
-		} else if day == 1 {
+		case 1:
 			fmt.Println("a Partridge in a Pear Tree")
 		}
 	}
+
+	// for day := 1; day <= 12; day++ {
+	// 	if day == 12 {
+	// 		fmt.Println("12 Drummers Drumming")
+	// 	} else if day == 11 {
+	// 		fmt.Println("11 pipers piping")
+	// 	} else if day == 10 {
+	// 		fmt.Println("Ten Lords a Leaping")
+	// 	} else if day == 9 {
+	// 		fmt.Println("Nine Ladies Dancing")
+	// 	} else if day == 8 {
+	// 		fmt.Println("Eight Maids a Milking")
+	// 	} else if day == 7 {
+	// 		fmt.Println("Seven Swans a Swimming")
+	// 	} else if day == 6 {
+	// 		fmt.Println("Six Geese a Laying")
+	// 	} else if day == 5 {
+	// 		fmt.Println("Five Golden Rings")
+	// 	} else if day == 4 {
+	// 		fmt.Println("Four Calling Birds")
+	// 	} else if day == 3 {
+	// 		fmt.Println("Three French Hens")
+	// 	} else if day == 2 {
+	// 		fmt.Println("Two Turtle Doves")
+	// 	} else if day == 1 {
+	// 		fmt.Println("a Partridge in a Pear Tree")
+	// 	}
+	// }
 }
