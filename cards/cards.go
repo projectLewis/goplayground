a6a8ac1c326a98b95f158e38ba7181d2ac2e274e
Seth Lewis <lewiscatenary@gmail.com>
2019-08-27 15:53:46 -0500

    used append

--- cards/cards.go
+++ cards/cards.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
